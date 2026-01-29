3af77f6d5ae44cdd197dbb2566007963848ee4e6
lbb-and-a-little-rabbit <915598980@qq.com>
2026-01-29 13:26:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# CMake练习
+系统 windows
+
+---
+## 运行方式（MinGW）
+```bash
+cmake -S . -B build -G "MinGW Makefiles"
+cmake --build build
+./build\*.exe
+```
---
+++ exercise1/head.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+using ll=long long;
+
+double divide(int a,int b);
+ll mult(int a,int b);
+int add(int a,int b);
+int sub(int a,int b);
